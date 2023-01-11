da262e34ad6652b0df82ac9a042d313e5e4e14e9
tarisahrahayu <tarisahrahayu20si@mahasiswa.pcr.ac.id>
2023-01-11 15:44:06 +0700

    Kalender

---
+++ app/src/main/java/com/example/rukost_kel10/MainActivity2.kt
@@ -0,0 +1,61 @@
+package com.example.rukost_kel10
+
+    import android.app.*
+    import android.content.Context
+    import android.content.Intent
+    import android.graphics.Color
+    import android.os.Build
+    import androidx.appcompat.app.AppCompatActivity
+    import android.os.Bundle
+    import androidx.core.app.NotificationCompat
+    import androidx.core.app.NotificationManagerCompat
+    import kotlinx.android.synthetic.main.metode_bayar.*
+
+    class MainActivity2 : AppCompatActivity() {
+
+        val CHANNEL_ID = "channelID"
+        val CHANNEL_NAME = "channelName"
+        val NOTIFICATION_ID = 0
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.metode_bayar)
+            buatNotifikasi()
+
+            val intent = Intent (this, MainActivity2::class.java)
+            val pendingIntent = TaskStackBuilder.create(this).run{
+                addNextIntentWithParentStack(intent)
+                getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+            }
+
+            val notification = NotificationCompat.Builder (this, CHANNEL_ID)
+                .setContentTitle("Kamar Berhasil Disewa!")
+                .setContentText("Mohon dicek kembali data diri!")
+                .setSmallIcon(R.drawable.ic_notif)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent)
+                .build()
+
+            val  notificationManager = NotificationManagerCompat.from(this)
+            btn_tglsewa.setOnClickListener {
+                notificationManager.notify(NOTIFICATION_ID, notification)
+            }
+
+        }
+
+        fun buatNotifikasi() {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val channel = NotificationChannel(
+                    CHANNEL_ID,
+                    CHANNEL_NAME,
+                    NotificationManager.IMPORTANCE_DEFAULT
+                )
+                    .apply {
+                        lightColor = Color.GREEN
+                        enableLights(true)
+                    }
+                val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                manager.createNotificationChannel(channel)
+            }
+        }
+    }
